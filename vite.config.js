09a608a57b8f16ffa286bb6466a44f787a7215f9
Amirhossien Lotfi <amirhossien.lotfi@nav.no>
2022-07-06 15:35:18 +0200

    henter assets fra prod

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,8 @@ import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 import { terser } from "rollup-plugin-terser";
 import { resolve } from "path";
 
-const reactUrl = "https://min-side-assets.dev.intern.nav.no/react/17/esm/index.js";
-const reactDomUrl = "https://min-side-assets.dev.intern.nav.no/react-dom/17/esm/index.js";
+const reactUrl = "https://min-side-assets.intern.nav.no/react/17/esm/index.js";
+const reactDomUrl = "https://min-side-assets.intern.nav.no/react-dom/17/esm/index.js";
 
 const imports = {
   react: reactUrl,
