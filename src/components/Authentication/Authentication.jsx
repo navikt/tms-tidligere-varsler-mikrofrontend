a9b4453aaaaaf58b7ce5caad24ea3815515cf7a2
Amirhossien Lotfi <amirhossien.lotfi@nav.no>
2023-01-19 13:28:06 +0100

    Bug fix

--- src/components/Authentication/Authentication.jsx
+++ src/components/Authentication/Authentication.jsx
@@ -5,9 +5,7 @@ import { fetcher } from "../../api/api";
 
 export const Authentication = ({ children }) => {
   const { data: status, isLoading, isError } = useQuery(apiStatusUrl, fetcher);
-  console.log(status);
   if (status?.authenticated === false || isError) {
-    console.log(status);
     window.location.assign(`${apiLoginUrl}?redirect_uri=${tidligereVarslerUrl}`);
     return null;
   }
