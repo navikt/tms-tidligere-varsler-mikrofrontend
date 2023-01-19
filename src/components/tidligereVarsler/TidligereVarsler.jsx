bcbcbd2608652ff58b8fefd6acea25a35a3ad807
Amirhossien Lotfi <amirhossien.lotfi@nav.no>
2023-01-19 09:28:54 +0100

    Sortere varsler med hooken useMemo, unngå sortering ved ny render.

--- src/components/tidligereVarsler/TidligereVarsler.jsx
+++ src/components/tidligereVarsler/TidligereVarsler.jsx
@@ -1,16 +1,18 @@
-import { Heading, Ingress, BodyShort, Tag } from "@navikt/ds-react";
+import { Heading, Ingress } from "@navikt/ds-react";
 import { useIntl } from "react-intl";
 import { setLocaleDate } from "../../utils/date";
 import Filter from "../filter/Filter";
 import globalStyle from "../../App.module.css";
 import style from "./TidligereVarsler.module.css";
 import { useQuery } from "react-query";
+import { useMemo } from "react";
 import { fetcher } from "../../api/api";
-import { inaktiveVarslerApiUrl, innloggingsstatusUrl } from "../../api/urls";
+import { inaktiveVarslerApiUrl } from "../../api/urls";
 import Varsel from "../varsel/Varsel";
 import { selectSearch, selectType } from "../../store/selectors";
 import useStore from "../../store/store";
 import EmptyVarselList from "../emptyVarselList/EmptyVarselList";
+import { byForstBehandlet } from "../../utils/date";
 
 const VarslingerPage = () => {
   const translate = useIntl();
@@ -18,7 +20,9 @@ const VarslingerPage = () => {
 
   const filterType = useStore(selectType);
   const filterSok = useStore(selectSearch);
-  const filtertedVarseler = varsler?.filter(
+
+  const sortedVarsler = useMemo(() => varsler?.sort(byForstBehandlet));
+  const filtertedVarseler = sortedVarsler?.filter(
     (varsel) => (filterType === "ALLE" || varsel.type === filterType) && varsel.tekst.toLowerCase().includes(filterSok)
   );
   const hasMaskedVarsel = varsler && varsler.some((varsel) => varsel.tekst === "");
