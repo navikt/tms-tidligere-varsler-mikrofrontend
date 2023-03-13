df9aeca8d71e70ed19d73ba6ac5180b16367a5f1
Eivinn Eilertsen <eivinne@Eivinns-MacBook-Pro.local>
2023-03-13 09:45:21 +0100

    La til en ny text.jsx fil som skal erstatte de andre språkfilene

---
+++ src/language/text.jsx
@@ -0,0 +1,72 @@
+export const text = {
+  tidligereVarslerOverskrift: {
+    nb: "Tidligere varsler",
+    nn: "Tidlegare varsel",
+    en: "Past notifications",
+  },
+  hoyereSikkerhetsnivaa: {
+    nb: "Du må logge inn med BankID, Buypass eller Commfides for å se alle varslene.",
+    nn: "Du må logge inn med BankID, Buypass eller Commfides for å sjå alle varsla.",
+    en: "Please log in with BankID, Buypass or Commfides to see all notifications.",
+  },
+  sokefeltLabel: {
+    nb: "Søk i varsler",
+    nn: "Søk i varsel",
+    en: "Search notifications",
+  },
+  filterToggleItemAlle: {
+    nb: "Alle",
+    nn: "Alle",
+    en: "All",
+  },
+  filterToggleItemOppgave: {
+    nb: "Oppgave",
+    nn: "Oppgåve",
+    en: "Task",
+  },
+  filterToggleItemBeskjed: {
+    nb: "Beskjed",
+    nn: "Beskjed",
+    en: "Message",
+  },
+  filterToggleItemInnboks: {
+    nb: "Innboks",
+    nn: "Innboks",
+    en: "Inbox",
+  },
+  varslerListTittelNyeVarsler: {
+    nb: "Nye varsler",
+    nn: "Nye varsel",
+    en: "New notifications",
+  },
+  varslerListTittelTidligereVarsler: {
+    nb: "Tidligere varsler",
+    nn: "Tidlegare varsel",
+    en: "Past notifications",
+  },
+  beskjedArkiverButtonLabel: {
+    nb: "Arkiver",
+    nn: "Arkiver",
+    en: "Archive",
+  },
+  beskjedMaskedTitle: {
+    nb: "Du har fått en beskjed, logg inn med høyere sikkerhetsnivå for å se meldingen.",
+    nn: "Du har fått ein beskjed, logg inn med høgare sikkerheitsnivå for å sjå beskjeden.",
+    en: "You have a message, please log in with a higher security level to read the message.",
+  },
+  oppgaveMaskedTitle: {
+    nb: "Du har fått en oppgave, logg inn med høyere sikkerhetsnivå for å se oppgaven.",
+    nn: "Du har fått ei oppgåve, logg inn med høgare sikkerheitsnivå for å sjå oppgåva.",
+    en: "You have a task, please log in with a higher security level to see the task.",
+  },
+  varslerIngenNyeVarsler: {
+    nb: "Du har ingen nye varsler",
+    nn: "Du har ingen nye varsel",
+    en: "You have no new messages.",
+  },
+  varslerViVarslerDeg: {
+    nb: "Vi varsler deg når noe skjer",
+    nn: "Vi varslar deg når noko skjer",
+    en: "We will notify you when something happens.",
+  },
+};
