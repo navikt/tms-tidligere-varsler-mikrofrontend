1d2419a5830c4eef71fc85e970652405ad7fbf8c
Amirhossien Lotfi <amirhossien.lotfi@nav.no>
2023-03-28 09:13:45 +0200

    Rydder eventListiner

--- src/provider/LanguageProvider.jsx
+++ src/provider/LanguageProvider.jsx
@@ -5,11 +5,19 @@ export const LanguageContext = createContext(defaultLanguage);
 
 const LanguageProvider = ({ children }) => {
   const [language, setLanguage] = useState(defaultLanguage);
+
   useEffect(() => {
-    window.addEventListener("storage", () => {
-      console.log("New event listener");
+    console.log("useEffect");
+    const handleStorageOnEvent = () => {
       setLanguage(sessionStorage.getItem("language") ?? "nb");
-    });
+      console.log("listiner ---");
+    };
+
+    window.addEventListener("storage", handleStorageOnEvent);
+
+    return () => {
+      window.removeEventListener("storage", handleStorageOnEvent);
+    };
   }, []);
 
   return <LanguageContext.Provider value={language}>{children}</LanguageContext.Provider>;
