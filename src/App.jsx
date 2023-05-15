7f83b7d5d570a396b449dcbeb7576c378da27c0b
Amirhossien Lotfi <amirhossien.lotfi@nav.no>
2023-05-15 09:02:18 +0200

    WIP, fjernet ekstra nivå i komponenter og endret størrelser og bakgrunnsfarge etter nytt design.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,59 @@
-import TidligereVarsler from "./components/tidligereVarsler/TidligereVarsler";
+import style from "./App.module.css";
+import { LanguageContext } from "./provider/LanguageProvider";
+import { useContext, useMemo } from "react";
+import { inaktiveVarslerApiUrl } from "./api/urls";
+import { fetcher } from "./api/api";
+import { byForstBehandlet } from "./utils/date";
+import { useQuery } from "react-query";
+import { Alert, Heading } from "@navikt/ds-react";
+import text from "./language/text";
+import Varsel from "./components/varsel/Varsel";
+import { selectSearch, selectType } from "./store/selectors";
+import useStore from "./store/store";
+import Filter from "./components/filter/Filter";
+
+const PageBody = ({ varsler, isSuccess }) => {
+  const filterType = useStore(selectType);
+  const filterSok = useStore(selectSearch);
+
+  const sortedVarsler = useMemo(() => varsler?.sort(byForstBehandlet));
+  const filtertedVarseler = sortedVarsler?.filter(
+    (varsel) =>
+      (filterType === "ALLE" || varsel.type === filterType) &&
+      (varsel.tekst === null || varsel.tekst.toLowerCase().includes(filterSok))
+  );
+
+  return (
+    <>
+      <Filter />
+      <ul className={`${style.varslerList}`}>
+        {isSuccess &&
+          filtertedVarseler?.map((varsel) => (
+            <li key={varsel.eventId}>
+              <Varsel varselData={varsel} />
+            </li>
+          ))}
+      </ul>
+    </>
+  );
+};
 
 function App() {
-  return <TidligereVarsler />;
+  const language = useContext(LanguageContext);
+  const { data: varsler, isSuccess } = useQuery(inaktiveVarslerApiUrl, fetcher);
+  const hasMaskedVarsel = varsler && varsler.some((varsel) => varsel.isMasked);
+
+  return (
+    <div className={style.app}>
+      <Heading size={"large"}>{text["tidligereVarslerOverskrift"][language]}</Heading>
+      {hasMaskedVarsel && (
+        <Alert className={style.hoyereSikkerhetsnivaaAlert} variant="info">
+          {text["hoyereSikkerhetsnivaa"][language]}
+        </Alert>
+      )}
+      {varsler?.length === 0 ? <EmptyVarselList /> : <PageBody varsler={varsler} isSuccess={isSuccess} />}
+    </div>
+  );
 }
 
 export default App;
--- src/components/filter/Filter.jsx
+++ src/components/filter/Filter.jsx
@@ -1,16 +1,12 @@
-import { Panel } from "@navikt/ds-react";
-import globalStyle from "../../App.module.css";
 import ToggleNotifikasjon from "./toggleNotifikasjon/ToggleNotifikasjon";
 import SearchField from "./searchField/SearchField";
 import style from "./Filter.module.css";
 
 const Filter = () => {
   return (
-    <div className={style.filterWrapper}>
-      <div className={`${style.filter} ${globalStyle.tekstInnhold}`}>
-        <SearchField />
-        <ToggleNotifikasjon />
-      </div>
+    <div className={style.filter}>
+      <SearchField />
+      <ToggleNotifikasjon />
     </div>
   );
 };
