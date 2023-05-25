153147b4a751c073dfc08c6fdae49103161180d1
Amirhossien Lotfi <amirhossien.lotfi@nav.no>
2023-05-25 22:36:33 +0200

    Fjernet ubrukt selector til store

--- src/store/selectors.js
+++ src/store/selectors.js
@@ -11,11 +11,3 @@ export const selectInnboks = (state) => state.innboks;
 export const selectSearch = (state) => state.filterSearch;
 
 export const updateSearch = (state) => state.search;
-
-export const selectBeskjedList = (state) => state.beskjedList;
-export const selectSetBeskjedList = (state) => state.setBeskjedList;
-export const selectRemoveBeskjed = (state) => state.removeBeskjed;
-
-export const selectInaktivBeskjedList = (state) => state.inaktivBeskjedList;
-export const selectSetInaktivBeskjedList = (state) => state.setInaktivBeskjedList;
-export const selectAddInaktivBeskjed = (state) => state.addInaktivBeskjedList;
