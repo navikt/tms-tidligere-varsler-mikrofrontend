7c40800f9cf1f7be58da01df86603de5fde8bb6c
Amirhossien Lotfi <amirhossien.lotfi@nav.no>
2023-05-25 22:34:34 +0200

    Refaktor

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,13 @@ import { inaktiveVarslerApiUrl } from "./api/urls";
 import { fetcher } from "./api/api";
 import { byForstBehandlet } from "./utils/date";
 import { useQuery } from "react-query";
-import { Alert, Heading } from "@navikt/ds-react";
+import { Alert, BodyLong, Heading, Loader } from "@navikt/ds-react";
 import text from "./language/text";
 import Varsel from "./components/varsel/Varsel";
 import { selectSearch, selectType } from "./store/selectors";
 import useStore from "./store/store";
 import Filter from "./components/filter/Filter";
+import EmptyVarselList from "./components/emptyVarselList/EmptyVarselList.jsx";
 
 const PageBody = ({ varsler, isSuccess }) => {
   const filterType = useStore(selectType);
@@ -24,7 +25,7 @@ const PageBody = ({ varsler, isSuccess }) => {
   );
 
   return (
-    <>
+    <div className={style.pageBody}>
       <Filter />
       <ul className={`${style.varslerList}`}>
         {isSuccess &&
@@ -34,24 +35,33 @@ const PageBody = ({ varsler, isSuccess }) => {
             </li>
           ))}
       </ul>
-    </>
+    </div>
   );
 };
 
 function App() {
   const language = useContext(LanguageContext);
-  const { data: varsler, isSuccess } = useQuery(inaktiveVarslerApiUrl, fetcher);
+  const { data: varsler, isSuccess, isLoading } = useQuery(inaktiveVarslerApiUrl, fetcher);
   const hasMaskedVarsel = varsler && varsler.some((varsel) => varsel.isMasked);
+  const visVarsel = false;
 
   return (
     <div className={style.app}>
-      <Heading size={"large"}>{text["tidligereVarslerOverskrift"][language]}</Heading>
-      {hasMaskedVarsel && (
-        <Alert className={style.hoyereSikkerhetsnivaaAlert} variant="info">
+      <Heading className={style.pageTitle} size={"large"}>
+        {text["tidligereVarslerOverskrift"][language]}
+      </Heading>
+      {!visVarsel && (
+        <Alert className={style.authenticationAlert} variant="info">
           {text["hoyereSikkerhetsnivaa"][language]}
         </Alert>
       )}
-      {varsler?.length === 0 ? <EmptyVarselList /> : <PageBody varsler={varsler} isSuccess={isSuccess} />}
+      {isLoading ? (
+        <Loader className={style.loader} size="3xlarge" title="venter..." />
+      ) : visVarsel ? (
+        <EmptyVarselList />
+      ) : (
+        <PageBody varsler={varsler} isSuccess={isSuccess} />
+      )}
     </div>
   );
 }
