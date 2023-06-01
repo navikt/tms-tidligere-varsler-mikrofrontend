238a14caaefe1144e6adc411d2f45fdb92705304
Amirhossien Lotfi <amirhossien.lotfi@nav.no>
2023-06-01 09:24:02 +0200

    Flyttet komponent TdiligereVarslerPage til egen fil

--- src/App.jsx
+++ src/App.jsx
@@ -3,53 +3,11 @@ import { LanguageContext } from "./provider/LanguageProvider";
 import { useContext, useMemo } from "react";
 import { inaktiveVarslerApiUrl } from "./api/urls";
 import { fetcher } from "./api/api";
-import { byForstBehandlet } from "./utils/date";
 import { useQuery } from "react-query";
-import { Alert, BodyLong, BodyShort, Heading, Loader } from "@navikt/ds-react";
+import { Alert, Heading, Loader } from "@navikt/ds-react";
 import text from "./language/text";
-import Varsel from "./components/varsel/Varsel";
-import { selectSearch, selectType } from "./store/selectors";
-import useStore from "./store/store";
-import Filter from "./components/filter/Filter";
 import NoTidligereVarslerPage from "./components/NoTidligereVarslerPage/NoTidligereVarslerPage.jsx";
-import TomSokKatt from "./assets/TomSokKatt";
-
-const TidligereVarslerPage = ({ varsler, isSuccess }) => {
-  const language = useContext(LanguageContext);
-  const filterType = useStore(selectType);
-  const filterSok = useStore(selectSearch);
-
-  const sortedVarsler = useMemo(() => varsler?.sort(byForstBehandlet));
-  const filtertedVarseler = sortedVarsler?.filter(
-    (varsel) =>
-      (filterType === "ALLE" || varsel.type === filterType) &&
-      (varsel.tekst === null || varsel.tekst.toLowerCase().includes(filterSok))
-  );
-
-  return (
-    <>
-      <Filter />
-      <BodyShort className={style.numberOfSearchHit}>
-        {text.antallSokeTreff(language, filtertedVarseler.length, varsler.length)}
-      </BodyShort>
-      {filtertedVarseler.length > 0 ? (
-        <ul className={style.varslerList}>
-          {isSuccess &&
-            filtertedVarseler?.map((varsel) => (
-              <li key={varsel.eventId}>
-                <Varsel varselData={varsel} />
-              </li>
-            ))}
-        </ul>
-      ) : (
-        <div className={style.emptySearchContainer}>
-          <TomSokKatt />
-          <BodyLong className={style.emptySearchDescription}>{text["ingenSokeresultat"][language]}</BodyLong>
-        </div>
-      )}
-    </>
-  );
-};
+import TidligereVarslerPage from "./components/TidligereVarslerPage/TidligereVarslerPage.jsx";
 
 function App() {
   const language = useContext(LanguageContext);
