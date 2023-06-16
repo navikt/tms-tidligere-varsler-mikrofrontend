45450360ec2e95ff8b6930a949f11de2711598c1
Amirhossien Lotfi <amirhossien.lotfi@nav.no>
2023-06-16 14:29:38 +0200

    Henter varsler direkte fra tms-varsel-api

--- src/api/urls.js
+++ src/api/urls.js
@@ -15,8 +15,8 @@ export const getEnvironment = () => {
 
 const TMS_VARSEL_API = {
   local: "http://localhost:3000/api/tms-varsel-api",
-  development: "https://www.intern.dev.nav.no/tms-min-side-proxy/varsel/tms-varsel-api",
-  production: "https://www.nav.no/tms-min-side-proxy/varsel/tms-varsel-api",
+  development: "https://www.intern.dev.nav.no/tms-varsel-api",
+  production: "https://www.nav.no/tms-varsel-api",
 };
 
 const MIN_SIDE_URL = {
