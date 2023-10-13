0effb61fc48fcdaa87c7c4115ae79c813c42fc07
Atle Nordland <atle.nordland@nav.no>
2023-10-13 13:52:40 +0200

    bump dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import Mikrofrontend from "./Mikrofrontend";
 import "./index.css";
 
-ReactDOM.render(
+const container = document.getElementById("root") as HTMLElement;
+const root = createRoot(container);
+
+root.render(
   <React.StrictMode>
     <div className="page-wrapper">
       <div className="page-layout">
@@ -13,5 +16,4 @@ ReactDOM.render(
       </div>
     </div>{" "}
   </React.StrictMode>,
-  document.getElementById("root")
 );
--- vitest-setup.ts
+++ vitest-setup.ts
@@ -1,4 +1,7 @@
 import "vitest-axe/extend-expect";
 import * as matchers from "vitest-axe/matchers";
-import { expect } from "vitest";
+import { expect, vi } from "vitest";
 expect.extend(matchers);
+
+// @ts-expect-error mock for å fikse jsdom-feil i testene
+HTMLCanvasElement.prototype.getContext = vi.fn();
