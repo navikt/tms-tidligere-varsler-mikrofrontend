311153768dc88bf4b83fc802e16327752e873385
Atle Nordland <atle.nordland@nav.no>
2023-10-13 14:33:30 +0200

    prettier-kjøring

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,11 +1,3 @@
-import { QueryFunctionContext } from "react-query";
-
-const checkResponse = (response: Response) => {
-  if (!response.ok) {
-    throw new Error("Fetch request failed");
-  }
-};
-
 export const fetcher = async (path: string) => {
   const response = await fetch(path, {
     method: "GET",
