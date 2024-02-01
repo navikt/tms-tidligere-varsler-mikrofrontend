b7aa94b71523f21304898832e8465827d175acdf
Amirhossien Lotfi <amirhossien.lotfi@nav.no>
2024-02-01 14:30:58 +0100

    Rydde package.json

--- src/Mikrofrontend.jsx
+++ src/Mikrofrontend.jsx
@@ -1,19 +1,15 @@
-import { QueryClientProvider } from "react-query";
 import App from "./App";
 import "./App.module.css";
 import LanguageProvider from "./provider/LanguageProvider";
 import { initAmplitude } from "./utils/amplitude";
-import queryClient from "./utils/query";
 
 const Mikrofrontend = () => {
   initAmplitude();
   
   return (
-    <QueryClientProvider client={queryClient}>
       <LanguageProvider>
         <App />
       </LanguageProvider>
-    </QueryClientProvider>
   );
 };
 
