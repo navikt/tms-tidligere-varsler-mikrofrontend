8e9a2cf5c2ecf5dd79d8fdfbadefe497f2cc08a8
Amirhossien Lotfi <Amirhossien.lotfi@nav.no>
2024-08-09 07:02:51 +0200

    fjerne urbukt kode

--- src/api/urls.ts
+++ src/api/urls.ts
@@ -1,5 +1,6 @@
 const isProduction = window.location.href.includes("www.nav.no");
-const isDevelopment = window.location.href.includes("www.intern.dev.nav.no") || window.location.href.includes("www.ansatt.dev.nav.no");
+const isDevelopment =
+  window.location.href.includes("www.intern.dev.nav.no") || window.location.href.includes("www.ansatt.dev.nav.no");
 
 export const getEnvironment = () => {
   if (isDevelopment) {
@@ -28,4 +29,3 @@ const MIN_SIDE_URL = {
 const tmsVarselAPI = TMS_VARSEL_API[getEnvironment()];
 export const minSideUrl = MIN_SIDE_URL[getEnvironment()];
 export const inaktiveVarslerApiUrl = `${tmsVarselAPI}/inaktive`;
-export const tidligereVarslerUrl = `${minSideUrl}/varslinger`;
